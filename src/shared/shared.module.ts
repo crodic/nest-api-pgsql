f0a171159f700880c4b13d64f4b8e028d2c8426a
phat.bien <phat.bien@manifera.com>
2026-03-29 08:15:50 +0000

    update

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { Global, Module } from '@nestjs/common';
-import { StorageService } from './services/storage.service';
 
 @Global()
 @Module({
   imports: [],
-  providers: [StorageService],
-  exports: [StorageService],
+  providers: [],
+  exports: [],
 })
 export class SharedModule {}
